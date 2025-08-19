e5c1b5c8d815163625ac4fc3468836f2ba5bff17
M-Unity <mihailsheshukov10@gmail.com>
2025-08-20 00:56:50 +0500

    RED

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -1,19 +1,30 @@
 using UnityEngine;
-
+using System;
+[Serializable]
+public struct Rooms
+{
+    public GameObject Room;
+    public GameObject Walls1;
+    public GameObject Walls2;
+}
 public class RoomManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] rooms;
+    [SerializeField] private Rooms[] roomes;
     private int currentIndex = 0;
 
     public void RotateRoom(int direction)
     {
-        transform.Rotate(Vector3.up, 90 * direction, 0);
+        transform.Rotate(Vector3.up, 180 * direction, 0);
+        print(transform.localEulerAngles.y);
+        roomes[currentIndex].Walls1.SetActive(transform.localEulerAngles.y == 0);
+        roomes[currentIndex].Walls2.SetActive(transform.localEulerAngles.y == 180);
     }
 
     public void ChangeRoom(int direction)
     {
-        rooms[currentIndex].SetActive(false);
-        currentIndex = (currentIndex + direction + rooms.Length) % rooms.Length;
-        rooms[currentIndex].SetActive(true);
+        roomes[currentIndex].Room.SetActive(false);
+        currentIndex = (currentIndex + direction + roomes.Length) % roomes.Length;
+        roomes[currentIndex].Room.SetActive(true);
     }
 }
